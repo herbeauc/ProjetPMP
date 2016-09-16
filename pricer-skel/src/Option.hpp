568930c5358ab122e80d3d22a55efa484a549bc7
Damien Samot <Damien.Samot@ensimag.grenoble-inp.fr>
2016-09-16 09:27:56 +0200

    test

--- pricer-skel/src/Option.hpp
+++ pricer-skel/src/Option.hpp
@@ -7,7 +7,7 @@
 class Option
 {
 public:
-    double T_; /// maturité
+    double T_; /// maturité !
     int nbTimeSteps_; /// nombre de pas de temps de discrétisation
     int size_; /// dimension du modèle, redondant avec BlackScholesModel::size_
     /**
